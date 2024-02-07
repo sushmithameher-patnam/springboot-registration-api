9331094be17928fdd3c293df6fec73421adfdade
mehersushu <patnamsushmithameher@gmail.com>
2024-02-07 16:49:38 -0600

    first commit

---
+++ src/test/java/com/example/smProject/SmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
